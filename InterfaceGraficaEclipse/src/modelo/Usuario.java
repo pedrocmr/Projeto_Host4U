6affe6fbb6e96f0cabc7b7d1a0c2750582da9ea8
Rafael Wilian <rafael.wilian2001@gmail.com>
2020-10-11 23:03:19 -0300

    colocando as alterações na nova branch

---
+++ InterfaceGraficaEclipse/src/modelo/Usuario.java
@@ -0,0 +1,63 @@
+package modelo;
+
+import java.util.ArrayList;
+
+public class Usuario {
+
+	//atributos
+	
+	private ArrayList<Cadastro> usuarios;
+	private int idUsuario;
+	
+	//construtores
+	public Usuario(ArrayList<Cadastro> usuarios) {
+		this.usuarios = usuarios;
+	}
+	
+	//métodos
+	public ArrayList<Cadastro> getUsuario() {
+		return usuarios;
+	}
+	
+	
+	public void setUsuarioNome(String nome){
+		this.usuarios.get(idUsuario).setNome(nome);
+	}
+	
+	public String getNome() {
+		return this.usuarios.get(idUsuario).getNome();
+	}
+	
+	public void setUsuarioLogin(String login) {
+		this.usuarios.get(idUsuario).setLogin(login);
+	}
+	
+	public String getLogin() {
+		return this.usuarios.get(idUsuario).getLogin();
+	}
+	
+	public void setUsuarioSenha(String senha) {
+		this.usuarios.get(idUsuario).setLogin(senha);
+	}
+	
+	public void setUsuarioSexo(String sexo) {
+		this.usuarios.get(idUsuario).setLogin(sexo);
+	}
+	
+	public String getSexo() {
+		return this.usuarios.get(idUsuario).getSexo();
+	}
+	
+	public String toString() {
+		return "Nome: " + getNome() + " " + 
+	    "Login: " + getLogin() + " " + 
+	    "Sexo: " + getSexo();
+	}
+	
+	
+	
+	
+	
+	
+	
+}
