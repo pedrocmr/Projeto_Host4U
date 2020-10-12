13d9ab0b1ecd5e7a288c017072edd39e8f66700f
Pedro Camara <57800238+pedrocmr@users.noreply.github.com>
2020-10-12 18:53:06 -0300

    classes que faltavam

---
+++ InterfaceGraficaEclipse/src/modelo/Administrador.java
@@ -0,0 +1,9 @@
+package modelo;
+
+public class Administrador {
+	private String nome;
+	private String senha;
+	
+	
+
+}
---
+++ InterfaceGraficaEclipse/src/modelo/Relatorio.java
@@ -0,0 +1,33 @@
+package modelo;
+
+public class Relatorio {
+ 
+	private Pagamento pagamento;
+	private Hotel hotel;
+	private Reserva reserva;
+	private Usuario usuario;
+	
+	
+	
+	public void relatorioIntervalo()
+	{
+		
+		
+	}
+	public void relatorioGeral()
+	{
+		
+		
+	}
+	public void relatorioMensal()
+	{
+		
+		
+	}
+	public String toString()
+	{
+		return " " ;
+	}
+	
+	
+}
