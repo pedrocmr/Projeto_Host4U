ce1ead82271bc38e29181b969173c0e8e1a86306
Eduardo Felipe <61950466+EduardoF90@users.noreply.github.com>
2020-10-22 16:05:03 -0300

    tela-meu-perfil-com-botao
    Implementação tela meu perfil usando o scene builder

--- InterfaceGraficaEclipse/src/gui/MeuPerfilController.java
+++ InterfaceGraficaEclipse/src/gui/MeuPerfilController.java
@@ -1,129 +1,83 @@
 package gui;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.paint.Color;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class MeuPerfilController extends Application {
+public class MeuPerfilController extends Application implements Initializable{
 
-	private TextField txLogin;
-	private PasswordField txSenha;
-	private TextField txNome;
-	private TextField txSexo;
+	private static Stage stage;
+	
+	 @FXML private Label texto6;
+	 @FXML private Label texto2;
+     @FXML private Label texto4;
+     @FXML private Label texto3;
+     @FXML private Label texto5;
+     @FXML private TextField txNome;
+	 @FXML private TextField txSenha;
+	 @FXML  private TextField txSexo;
+	 @FXML private TextField txLogin;
+     @FXML private Button btSalvar;
+     @FXML private Rectangle retangulo;
+     @FXML private Label texo1;
+     @FXML private ImageView imagem1;
+     @FXML private Button btVoltar;
+     @FXML private AnchorPane rootPane;
+     
+     
+     @FXML
+     void funcionaBotao(ActionEvent event) {
+      
+    	 System.out.println("salvei!");
+     }
+     
+	public void start(Stage stage) throws Exception {
 
-	public static void main(String args[]) {
+		// CRIANDO LAYOUT
+		AnchorPane root = FXMLLoader.load(getClass().getResource("MeuPerfil.fxml"));
+		Scene scene = new Scene(root, 700, 500);
 
-		launch(args);
+		// COLOCANDO TÍTULO
+		stage.setTitle("HOST4U - Meu Perfil");
+
+		// GERANDO TELA
+		stage.setResizable(false);
+		stage.setScene(scene);
+		stage.show();
+		
+		setStage(stage);
 
 	}
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
+	public static Stage getStage() {
+		return stage;
+	}
 
-		// CRIANDO LAYOUT
-		Group layout = new Group();
-		Scene scene = new Scene(layout, 700, 550);
-
-		// COLOCANDO TÍTULO
-		primaryStage.setTitle("HOST4U - Meu Perfil");
-
-		// COLOCANDO TEXTOS
-		Text titulo = new Text("MEU PERFIL");
-		titulo.setLayoutX(10);
-		titulo.setLayoutY(70);
-		titulo.setFont(new Font("Comic Sans MS", 24));
-		layout.getChildren().add(titulo);
-
-		Text texto1 = new Text("Nome:");
-		texto1.setLayoutX(200);
-		texto1.setLayoutY(130);
-		layout.getChildren().add(texto1);
-
-		Text texto2 = new Text("Login:");
-		texto2.setLayoutX(200);
-		texto2.setLayoutY(190);
-		layout.getChildren().add(texto2);
-
-		Text texto3 = new Text("Senha:");
-		texto3.setLayoutX(200);
-		texto3.setLayoutY(250);
-		layout.getChildren().add(texto3);
-
-		Text texto4 = new Text("Sexo:");
-		texto4.setLayoutX(210);
-		texto4.setLayoutY(310);
-		layout.getChildren().add(texto4);
+	public static void setStage(Stage stage) {
+		MeuPerfilController.stage = stage;
+	}
+	
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
 		
-		Label lb_reserva = new Label("Minhas Reservas:");
-		lb_reserva.setFont(new Font("Arial Black",20));
-		lb_reserva.setLayoutX(435);
-		lb_reserva.setLayoutY(95);
-		layout.getChildren().add(lb_reserva);
+	}
+	
+	public static void main(String args[]) {
 
-		// INICIALIZANDO CAIXA DE TEXTO
-		txNome = new TextField();
-		txNome.setPrefWidth(210);
-		txNome.setPromptText("Nome");
-		txNome.setLayoutX(200);
-		txNome.setLayoutY(140);
-
-		txLogin = new TextField();
-		txLogin.setPrefWidth(210);
-		txLogin.setPromptText("Login");
-		txLogin.setLayoutX(200);
-		txLogin.setLayoutY(200);
-
-		txSenha = new PasswordField();
-		txSenha.setPrefWidth(210);
-		txSenha.setPromptText("Senha");
-		txSenha.setLayoutX(200);
-		txSenha.setLayoutY(260);
-
-		txSexo = new TextField();
-		txSexo.setPrefWidth(210);
-		txSexo.setPromptText("Sexo");
-		txSexo.setLayoutX(200);
-		txSexo.setLayoutY(320);
-
-		// INICIALIZANDO IMAGE E ADICIONANDO ICONE
-		Image imagem = new Image("imagens/iconehotel.png");
-		primaryStage.getIcons().add(imagem);
-		
-		ImageView imagem1 = new ImageView("imagens/PerfilAzul.png");
-		imagem1.setLayoutX(10);
-		imagem1.setLayoutY(100);
-		imagem1.fitHeightProperty().set(145);
-		imagem1.fitWidthProperty().set(155);
-		
-		// INICIALIZANDO BOTÃO
-		Button botao = new Button("Concluir");
-		botao.setLayoutX(310);
-		botao.setLayoutY(500);
-		
-		//RETANGULO
-		Rectangle retangulo = new Rectangle(430,90,250,330);
-        retangulo.setFill(null);
-        retangulo.setStroke(Color.BLACK);
-        
-		
-		// ADICIONANDO ELEMENTOS NA TELA
-		layout.getChildren().addAll(botao, txNome, txLogin, txSenha, txSexo, imagem1 , retangulo);
-
-		// GERANDO TELA
-		primaryStage.setResizable(false);
-		primaryStage.setScene(scene);
-		primaryStage.show();
+		launch(args);
 
 	}
 
--- InterfaceGraficaEclipse/src/repositorio/RepositorioReservaArray.java
+++ InterfaceGraficaEclipse/src/repositorio/RepositorioReservaArray.java
@@ -16,52 +16,49 @@ public class RepositorioReservaArray implements IRepositorioReserva {
 	@Override
 	public boolean addReserva(Reserva novaReserva) {
 		boolean resultado = false;
-		
+
 		if (novaReserva != null) {
-			
+
 			boolean jaReservada = false;
-			
+
 			for (Reserva nova : reservas) {
 
 				if (nova.getQuarto().getHotel().equals(novaReserva.getQuarto().getHotel()) == true) {
-					
-				
-					if(nova.getQuarto().getNumero() == novaReserva.getQuarto().getNumero()){
-					
-					if((novaReserva.getCheckin().isBefore(nova.getCheckin()) == true) &&
-						(novaReserva.getCheckout().isBefore(nova.getCheckin()) == true) ||
-						novaReserva.getCheckin().isAfter(nova.getCheckout()) == true) {
-						
-						reservas.add(novaReserva);
-						
-						}else {
-							
+
+					if (nova.getQuarto().getNumero() == novaReserva.getQuarto().getNumero()) {
+
+						if ((novaReserva.getCheckin().isBefore(nova.getCheckin()) == true)
+								&& (novaReserva.getCheckout().isBefore(nova.getCheckin()) == true)
+								|| novaReserva.getCheckin().isAfter(nova.getCheckout()) == true) {
+
+							reservas.add(novaReserva);
+
+						} else {
+
 							jaReservada = true;
 						}
-					
+
 					}
-					
-					
-					if(jaReservada == false) {
-						
+
+					if (jaReservada == false) {
+
 						reservas.add(novaReserva);
-					
-					}else {
-						
-						if(reservas.contains(novaReserva)) {
-							
-							reservas.remove(novaReserva);						}
+
+					} else {
+
+						if (reservas.contains(novaReserva)) {
+
+							reservas.remove(novaReserva);
+						}
 					}
-				
+
 				}
-				
+
 			}
 		}
-		
-		
-		
+
 		return resultado;
-		
+
 	}
 
 	@Override
