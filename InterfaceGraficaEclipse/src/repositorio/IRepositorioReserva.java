4fa5e8387ae82b363c9d2d946167acc34794815d
Eduardo Felipe <61950466+EduardoF90@users.noreply.github.com>
2020-10-23 21:53:25 -0300

    alteração_telas_repositorio
    Criação de tela "Minhas Reservas" e altereção na tela de "Meu Perfil" , além de alterações no repositorio de reserva.

--- InterfaceGraficaEclipse/src/repositorio/IRepositorioReserva.java
+++ InterfaceGraficaEclipse/src/repositorio/IRepositorioReserva.java
@@ -1,14 +1,18 @@
 package repositorio;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 import modelo.Reserva;
+import modelo.Usuario;
 
 public interface IRepositorioReserva {
 
 	boolean addReserva(Reserva reserva);
-	
+
 	boolean cancelarReserva(Reserva reservaCancela);
-	
-	void listarReservas();
-	
-	void remarcarReserva();
+
+	ArrayList<Reserva> listarReservasUsuario(Usuario usuario);
+
+	void remarcarReserva(LocalDate checkin, LocalDate checkout);
 }
--- InterfaceGraficaEclipse/src/repositorio/RepositorioReservaArray.java
+++ InterfaceGraficaEclipse/src/repositorio/RepositorioReservaArray.java
@@ -1,9 +1,11 @@
 package repositorio;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 import modelo.Reserva;
+import modelo.Usuario;
 
 public class RepositorioReservaArray implements IRepositorioReserva {
 
@@ -17,79 +19,86 @@ public class RepositorioReservaArray implements IRepositorioReserva {
 	@Override
 	public boolean addReserva(Reserva novaReserva) {
 		boolean resultado = false;
-		
+
 		if (novaReserva != null) {
-			
+
 			boolean jaReservada = false;
-			
+
 			for (Reserva nova : reservas) {
 
 				if (nova.getQuarto().getHotel().equals(novaReserva.getQuarto().getHotel()) == true) {
-					
-				
-					if(nova.getQuarto().getNumero() == novaReserva.getQuarto().getNumero()){
-					
-					if((novaReserva.getCheckin().isBefore(nova.getCheckin()) == true) &&
-						(novaReserva.getCheckout().isBefore(nova.getCheckin()) == true) ||
-						novaReserva.getCheckin().isAfter(nova.getCheckout()) == true) {
-						
-						}else {
-							
+
+					if (nova.getQuarto().getNumero() == novaReserva.getQuarto().getNumero()) {
+
+						if ((novaReserva.getCheckin().isBefore(nova.getCheckin()) == true)
+								&& (novaReserva.getCheckout().isBefore(nova.getCheckin()) == true)
+								|| novaReserva.getCheckin().isAfter(nova.getCheckout()) == true) {
+
+						} else {
+
 							jaReservada = true;
 						}
-					
+
 					}
-					
-					
-					if(jaReservada == false) {
-						
+
+					if (jaReservada == false) {
+
 						reservas.add(novaReserva);
-					
+
 						resultado = true;
 					}
-							
+
 				}
-				
+
 			}
 		}
-		
-		
-		
+
 		return resultado;
-		
+
 	}
 
 	@Override
 	public boolean cancelarReserva(Reserva reservaCancela) {
 
-			
 		boolean resultado = false;
-		
+
 		for (Iterator<Reserva> iterator = reservas.iterator(); iterator.hasNext();) {
-				
+
 			Reserva existente = iterator.next();
-				
-				if(existente.equals(reservaCancela)) {
-					
-					iterator.remove();
-					resultado = true;
-				}
+
+			if (existente.equals(reservaCancela)) {
+
+				iterator.remove();
+				resultado = true;
 			}
-		
+		}
+
 		return resultado;
 	}
 
 	@Override
-	public void listarReservas() {
-		for (Reserva r : reservas) {
-			System.out.println(r);
-		}
+	public ArrayList<Reserva> listarReservasUsuario(Usuario usuario) {
 
+		ArrayList<Reserva> reserva = new ArrayList<Reserva>();
+
+		for (Reserva e : reservas) {
+
+			if (e.getUsuario().equals(usuario)) {
+
+				reserva.add(e);
+			}
+		}
+		return reserva;
 	}
 
 	@Override
-	public void remarcarReserva() {
+	public void remarcarReserva(LocalDate checkin, LocalDate checkout) { // PRECISA ADICIONAR AS CONDIÇÕES
 
+		for (Reserva r : reservas) {
+
+			r.setCheckin(checkin);
+			r.setCheckout(checkout);
+		}
 	}
 
 }
