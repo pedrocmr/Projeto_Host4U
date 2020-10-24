8453c7f2f4b455327914897139657a5466a182e3
Renan Paixão <renanlemos21@gmail.com>
2020-10-24 13:50:25 -0300

    alterações de controle cadastro e controle reserva refeitas, repo e tambem

--- InterfaceGraficaEclipse/src/controlador/ControleReserva.java
+++ InterfaceGraficaEclipse/src/controlador/ControleReserva.java
@@ -1,41 +1,120 @@
 package controlador;
 
 import java.time.LocalDate;
+import java.util.Iterator;
 
-import modelo.Quarto;
 import modelo.Reserva;
 import modelo.Usuario;
+import repositorio.RepositorioReservaArray;
 
 public class ControleReserva {
 
-	
+	RepositorioReservaArray repo;
 
 	public ControleReserva() {
 		
+		repo = new RepositorioReservaArray();
 	}
 
 	// MÉTODO PARA PREENCHER RESERVAS
-	public void preencheReserva(Usuario usuario, LocalDate checkin, LocalDate checkout) {
+	public boolean addReserva(Reserva novaReserva) {
+		
+		boolean resultado = false;
+		
+		if (novaReserva != null) {
+			
+			boolean jaReservada = false;
+			
+			for (Reserva existente : repo.getReservas()) {
 
-		quarto.getReserva().add(new Reserva(usuario, checkin, checkout));
+				if (existente.getQuarto().getHotel().equals(novaReserva.getQuarto().getHotel()) == true) {
+					
+				
+					if(existente.getQuarto().getNumero() == novaReserva.getQuarto().getNumero()){
+					
+						if((novaReserva.getCheckin().isBefore(existente.getCheckin()) == true) &&
+							(novaReserva.getCheckout().isBefore(existente.getCheckin()) == true) ||
+							novaReserva.getCheckin().isAfter(existente.getCheckout()) == true) {
+						
+						}else {
+							
+							jaReservada = true;
+						}
+					}
+						
+					if(jaReservada == false) {
+						
+						repo.getReservas().add(novaReserva);
+					
+						resultado = true;
+					}			
+				}
+			}
+		}
+		
+		
+		
+		return resultado;
+		
 
 	}
 
 	// MÉTODO CANCELAR RESERVAS
-	public void cancelaReserva(LocalDate checkin) {
-
+	public boolean cancelaReserva(Usuario usuario, Reserva reservaCancela) {
+		
+		if(reservaCancela == null) { 
+			
+			return false;
+		}
+			
+		boolean resultado = false;
+		
+		for (Iterator<Reserva> iterator = repo.getReservas().iterator(); iterator.hasNext();) {
+				
+			Reserva existente = iterator.next();
+				
+				if(existente.equals(reservaCancela)) {
+					
+					iterator.remove();
+					resultado = true;
+				}
+			}
+		
+		return resultado;
 		
 	}
 
 	// MÉTODO REMARCAR RESERVAS
-	public void remarcarReserva(LocalDate checkin, LocalDate checkout) {
+	public boolean remarcarReserva(Usuario usuario, Reserva reservaCancela, Reserva reservaRemarca) {
 
-		for (int i = 0; i < quarto.getReserva().size(); i++) {
-
-			quarto.getReserva().get(i).setCheckin(checkin);
-			quarto.getReserva().get(i).setCheckout(checkout);
-
+		if (usuario == null || reservaCancela == null || reservaRemarca == null) {
+			
+			return false;
 		}
+		
+		boolean resultado = false;
+		
+		if (cancelaReserva(usuario, reservaCancela)) {
+			
+			//cadastrado
+			
+			if (addReserva(reservaRemarca)) {
+				
+				resultado = true;
+			}
+		
+		}/*else {
+			
+			naoCadastrado = true;
+		}
+		
+		boolean naoCadastrado = false;
+		
+		if(naoCadastrado) { 
+		 * 
+		 * //posso alterar pra tentar mostrar algum erro na tela, dizendo que nao ha cadastro
+		}*/
+		
+		return resultado;
 	}
-
 }
